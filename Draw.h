1f10505668d39b07bc2d0264558a85ba963e800d
PolyPhaseFilter <43889628+PolyPhaseFilter@users.noreply.github.com>
2018-11-09 20:15:44 -0500

    Add files via upload

---
+++ Draw.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Bitmap.h"
+
+#ifndef __DRAW_H__
+#define __DRAW_H__
+/*
+this is the progream for bitmap realization
+
+author: Hongbo Li		Jiang Li
+date:	11/6/2018
+*/
+
+
+
+#endif
---
+++ Fill.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#ifndef __FILL_H__
+#define __FILL_H__
+
+
+#include "Bitmap.h"
+
+/*
+this is the progream for bitmap realization
+
+author: Hongbo Li		Jiang Li
+date:	11/6/2018
+*/
+
+
+
+#endif
